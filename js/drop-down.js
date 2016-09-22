2e6ebb0dfa2f68c22671b346d2c93ce54e404bcd
1992sam <1992.sameer@gmail.com>
2016-09-22 19:22:41 +1000

    Create drop-down.js

---
+++ js/drop-down.js
@@ -0,0 +1,11 @@
+function slider1funtion(newValue)
+{}
+function slider2funtion(newValue)
+{}
+function showMap()
+{}
+function resizeIframe(iFrame) {
+	
+    iFrame.style.height = iFrame.contentWindow.document.body.scrollHeight;
+	iFrame.style.Width = iFrame.contentWindow.document.body.scrollWidth;
+  }
